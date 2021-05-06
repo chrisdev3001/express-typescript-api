c5385609ccb5787a718254599d699cf330283c66
Chris Villarroel <cvillarroel@itaum.com>
2021-05-05 23:16:05 -0400

    Updated .gitignore for builds

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,12 +6,12 @@ const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 1337;
 
 const SERVER = {
-  hostname: SERVER_HOSTNAME,
-  port: SERVER_PORT
+    hostname: SERVER_HOSTNAME,
+    port: SERVER_PORT
 };
 
 const config = {
-  server: SERVER
+    server: SERVER
 };
 
 export default config;
